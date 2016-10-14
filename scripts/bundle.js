29bfefeef117685434c0d3ab023a0e3c915a2c00
devsheder <adrien.lasselle@gmail.com>
2016-10-14 12:04:04 +0200

    Update bundle.js

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -1,1 +1,15 @@
 /**/
+window.onload = function () {
+  var windowHeight = parseInt(window.screen.height);
+  var windowWidth = parseInt(window.screen.width);
+  var nbColumns = 20;
+  
+  // on détermine la largeur du background de chaque élément
+  var widthBg = Math.trunc(windowWidth / 20);
+  
+  // on détermine le nombre de ligne en fonction de largeur pour chaque background soit carré
+  var nbRows = Math.trunc(windowHeight / widthBg);
+  
+  console.log(widthBg);
+  console.log(nbRows);
+};
